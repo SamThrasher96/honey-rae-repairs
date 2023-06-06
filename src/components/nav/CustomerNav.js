e3b174fe7d0b8b5bbae464b7780bd3ea955fbd96
Sam Thrasher <samthrasher9@gmail.com>
2023-06-06 13:55:08 -0500

    Finish Honey rae ch 11

--- src/components/nav/CustomerNav.js
+++ src/components/nav/CustomerNav.js
@@ -1,16 +1,13 @@
 import { Link, useNavigate } from "react-router-dom"
 import "./NavBar.css"
 
-export const EmployeeNav = () => {
+export const CustomerNav = () => {
     const navigate = useNavigate()
 
     return (
         <ul className="navbar">
             <li className="navbar__item active">
                 <Link className="navbar__link" to="/tickets">Tickets</Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/employees">Employees</Link>
             </li>
             {
                 localStorage.getItem("honey_user")
